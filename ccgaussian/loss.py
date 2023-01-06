2dcaab0d84260e9c9074847feb54b69bd1e44c90
PatrickFeeney <patrickfeeney1225@gmail.com>
2023-01-06 12:54:11 -0500

    Fix Unsup Loss

--- ccgaussian/loss.py
+++ ccgaussian/loss.py
@@ -36,50 +36,69 @@ class NDCCLoss(torch.nn.Module):
 
 class UnsupNLLLoss(torch.nn.Module):
     def __init__(self, num_classes, num_batches) -> None:
+        """Unsupervised GMM NLL loss
+
+        Args:
+            num_classes (int): Number of GMM clusters K
+            num_batches (int): Number of batches per epoch for max queue size
+        """
         super().__init__()
-        # queue for calculating mixing coefficients, up to num_batches x K
+        # queue for calculating mixing coefficients, up to N x K
         self.log_resp_queue = torch.empty(0, num_classes)
         # mixing coefficient used for initialization epoch
         self.init_mix_coef = torch.log(torch.Tensor([1 / num_classes] * num_classes))
-        # current mixing coefficient, K
+        # current mixing coefficients, size K
         self.log_mix_coef = self.init_mix_coef
-        # number of batches per epoch, max queue size
-        self.num_batches = num_batches
+        # max queue size
+        self.max_batches = num_batches
+        # number of batches queued currently
+        self.cur_batches = 0
 
     def all_ll(self, embeds, means, sigma2s):
-        # GMM log likelihood for each cluster, B x K
+        # GMM log likelihood for each cluster, output B x K
         cc_ll = -torch.log(2 * torch.scalar_tensor(torch.pi)) * (len(sigma2s) / 2) \
             - torch.log(sigma2s).sum() / 2 \
             - all_mahalanobis(embeds, means, sigma2s) / 2
-        if self.log_resp_queue.shape[0] == self.num_batches:
+        # use learned mixing coef if queue has been filled
+        if self.cur_batches == self.max_batches:
             log_mix_coef = self.log_mix_coef
         else:
             log_mix_coef = self.init_mix_coef
         return log_mix_coef + cc_ll
 
-    def batch_log_resp(self, all_ll):
-        # B x K to B x K
-        return all_ll - torch.logsumexp(all_ll, dim=1, keepdim=True)
-
     def update_resp(self, batch_log_resp):
-        batch_len = batch_log_resp.shape[0]
-        # enqueue
-        log_resp_sum = torch.logsumexp(batch_log_resp, dim=0) \
-            - torch.log(torch.scalar_tensor(batch_len))
-        self.log_resp_queue = torch.vstack((self.log_resp_queue, log_resp_sum))
-        self.log_mix_coef += log_resp_sum
+        # update mixing coefficient and responsibility queue
+        # enqueue batch of responsiblities B x K
+        self.log_resp_queue = torch.vstack((self.log_resp_queue, batch_log_resp))
+        self.cur_batches += 1
         # dequeue if full
-        if self.log_resp_queue.shape[0] > self.num_batches:
-            self.log_mix_coef -= self.log_resp_queue[0]
-            self.log_resp_queue = self.log_resp_queue[1:]
+        if self.cur_batches > self.max_batches:
+            self.log_resp_queue = self.log_resp_queue[batch_log_resp.shape[0]:]
+            self.cur_batches -= 1
+        # update mixing coefficient, mean of the queued entries in log space
+        self.log_mix_coef = torch.logsumexp(self.log_resp_queue, dim=0) \
+            - torch.log(torch.scalar_tensor(self.log_resp_queue.shape[0]))
 
     def forward(self, embeds, means, sigma2s):
+        """Unsupervised GMM NLL loss
+
+        Args:
+            embeds (torch.Tensor): Latent space embeddings (B, D)
+            means (torch.Tensor): Cluster means (K, D)
+            sigma2s (torch.Tensor): Cluster independent covariance diagonal (D,)
+
+        Returns:
+            float: Loss value
+        """
         all_ll = self.all_ll(embeds, means, sigma2s)
-        batch_log_resp = self.batch_log_resp(all_ll)
+        # log likelihood of each input, B x 1
+        total_ll = torch.logsumexp(all_ll, dim=1, keepdim=True)
+        # cluster log responsibility for each input, B x K
+        batch_log_resp = all_ll - total_ll
         # check queue size before updating
-        filling_queue = self.log_resp_queue.shape[0] < self.num_batches
+        filling_queue = self.cur_batches < self.max_batches
         self.update_resp(batch_log_resp)
         # stop after updating queue until initialized from full epoch
         if filling_queue:
             return torch.scalar_tensor(0)
-        return -torch.logsumexp(all_ll, dim=1).sum()
+        return -total_ll.sum()
--- tests/test_loss.py
+++ tests/test_loss.py
@@ -1,19 +1,38 @@
+import pytest
 import torch
 
-import ccgaussian.loss as loss
+import ccgaussian.loss
 
 
 batch_size = 8
-embed_dim = 768
-num_classes = 5
-num_batches = 3
+embed_dim = 2
+num_classes = 2
+num_batches = 5
 
 
-def test_unsup_nll():
-    unsup_loss = loss.UnsupNLLLoss(num_classes, num_batches)
-    embeds = torch.randn((batch_size, embed_dim))
-    means = torch.randn((num_classes, embed_dim))
-    sigma2s = torch.ones((embed_dim,))
-    for _ in range(num_batches):
-        assert unsup_loss(embeds, means, sigma2s) == 0
-    assert unsup_loss(embeds, means, sigma2s) != 0
+class TestUnsupNLL():
+    @pytest.fixture
+    def loss(self):
+        return ccgaussian.loss.UnsupNLLLoss(num_classes, num_batches)
+
+    def test_queue_delay(self, loss):
+        embeds = torch.randn((batch_size, embed_dim))
+        means = torch.randn((num_classes, embed_dim))
+        sigma2s = torch.ones((embed_dim,))
+        for _ in range(num_batches):
+            assert loss(embeds, means, sigma2s) == 0
+        assert loss(embeds, means, sigma2s) != 0
+
+    def test_nll(self, loss):
+        embeds = torch.Tensor([[0, 0], [1, 0], [-1, 0]])
+        means = torch.Tensor([[1, 0], [-1, 0]])
+        sigma2s = torch.ones((embed_dim,))
+        for _ in range(num_batches):
+            loss(embeds, means, sigma2s)
+        all_ll = loss.all_ll(embeds, means, sigma2s)
+        total_ll = torch.logsumexp(all_ll, dim=1)
+        # construct true mixture
+        true_mix = torch.distributions.MixtureSameFamily(
+            torch.distributions.Categorical(torch.ones(2)),
+            torch.distributions.MultivariateNormal(means, torch.diag(sigma2s)))
+        assert torch.allclose(true_mix.log_prob(embeds), total_ll)
